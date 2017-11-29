0d4c4a3f598ec4aecc9745f4536b953360d303cd
cristian3629 <cristian.gonzalez@despegar.com>
2017-11-29 12:24:14 -0300

    ya se obtiene el token

--- src/pagos.js
+++ src/pagos.js
@@ -1,27 +1,43 @@
 var axios = require("axios");
+var request = require('request');
 
-var token = (async function () {
 
-    var headers = {
-      "Content-Type": "application/json"
+var token;
+
+var headers = {
+  "Content-Type": "application/json"
+}
+
+
+var options = {
+    url: 'http://shielded-escarpment-27661.herokuapp.com/api/v1/user/oauth/authorize',
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify({client_id:'ce69d9a7-226a-4b43-ae8b-50f6609c6738', client_secret: '59a8011a-47a6-404e-8633-097256c981ca'})
+}
+
+request(options, function (error, response, body) {
+    if (!error && response.statusCode == 201) {
+        // Print out the response body
+        console.log(body)
+        token = body['access_token']
+        console.log('Se obtuvo el token');
     }
-    var data = {
-      client_id: "ce69d9a7-226a-4b43-ae8b-50f6609c6738",
-      client_secret: "59a8011a-47a6-404e-8633-097256c981ca"
-    }
-
-    await axios.post("shielded-escarpment-27661.herokuapp.com/api/v1/user/oauth/authorize",data,headers);
-})()['access_token']
+    console.log('statusCode:'+response.statusCode);
+    console.log('body:'+body);
+})
 
 
 
 var api = axios.create({
-    baseURL: "shielded-escarpment-27661.herokuapp.com/api/v1/",
+    baseURL: "http://shielded-escarpment-27661.herokuapp.com/api/v1/",
     headers: {
         "Content-Type": "application/json",
-        "Authorization": "Bearer " + token
+        "Authorization": "Bearer "+token
     }
 })
+
+
 
 module.exports = {
     getPaymethods: async () => {
